ac36b3d1679637480d2fc8e1c79750ea23073291
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-05-10 19:28:33 +0530

    backend using Springboot setup completed

---
+++ backend_nike/src/main/java/com/nike_clone/backend_nike/BackendNikeApplication.java
@@ -0,0 +1,13 @@
+package com.nike_clone.backend_nike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNikeApplication.class, args);
+	}
+
+}
---
+++ backend_nike/src/test/java/com/nike_clone/backend_nike/BackendNikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nike_clone.backend_nike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

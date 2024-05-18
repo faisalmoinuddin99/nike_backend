ba0aba015d0acd742d3f6702e59d39314791148e
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-05-18 20:50:21 +0530

    get shoe by id

--- backend_nike/src/main/java/com/nike_clone/backend_nike/controller/ShoeController.java
+++ backend_nike/src/main/java/com/nike_clone/backend_nike/controller/ShoeController.java
@@ -1,12 +1,13 @@
 package com.nike_clone.backend_nike.controller;
 
 import com.nike_clone.backend_nike.model.Shoe;
-import com.nike_clone.backend_nike.service.ShoeService;
 import com.nike_clone.backend_nike.service.ShoeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/api")
@@ -23,6 +24,11 @@ public class ShoeController {
     @GetMapping
     public List<Shoe> getShoeList() {
         return service.getAllShoes();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Shoe> getShoeById(@PathVariable UUID id) {
+        return service.getSingleShoeById(id);
     }
 
 }
